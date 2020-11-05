013eab68aea5e21be89faf24fec0fd97ba6e26bc
Socks-code <55949458+Socks-code@users.noreply.github.com>
2020-11-05 13:04:06 -0500

    Add files via upload

---
+++ Mailbox Alert System/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+
+// or:
+// $username = $_POST['username']
+// $password = $_POST['password']
+?>
